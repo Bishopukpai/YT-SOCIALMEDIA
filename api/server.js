7fc5d866d9e3d2235b81ecf5330bc597ec131992
Bishop ukpai <bishopukpai0@gmail.com>
2024-08-27 09:46:03 +0100

    made the application to accept json requests and return json responses

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 require('./config/databaseConfig')
+const helmet = require('helmet');
+const morgan = require('morgan')
+
 
 const app = express()
 
 const port = 9090;
+
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("dev"))
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
