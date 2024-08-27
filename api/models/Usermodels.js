d000608818df22f2c5cd2b28b0ab9eb5dc45ef87
Bishop ukpai <bishopukpai0@gmail.com>
2024-08-27 10:57:25 +0100

    Created the user model

---
+++ api/models/Usermodels.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true
+    },
+    email: {
+       type: String,
+       required: true,
+       max: 50,
+       unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8,   
+    },
+    dateOfbirth: {
+        type: Date,  
+    },
+    profilephoto: {
+        type: String,
+        default: ""
+    },
+    followers: {
+        type: Array,
+        default: []
+    },
+    followings: {
+        type: Array,
+        default: []
+    },
+    description: {
+        type: String,
+        max: 100   
+    },
+    currentcity : {
+        type: String,
+        max: 50
+    },
+    from: {
+        type: String,
+        max: 50
+    },
+    worksAt: {
+        type: String,
+        max: 100
+    },
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model("User", UserSchema)
