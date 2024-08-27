3953df428d93372a4f5d5c8a9144618fd068a782
Bishop ukpai <bishopukpai0@gmail.com>
2024-08-27 17:22:37 +0100

    created the signup endpointand added error handling for empty fields

--- api/routes/Signup.js
+++ api/routes/Signup.js
@@ -1,3 +1,19 @@
 const router = require('express').Router()
 
+router.post("/signup", async(req, res) => {
+    let {fullname, username, email, password, dateOfbirth} = req.body
+
+    fullname = fullname.trim()
+    username = username.trim()
+    email = email.trim()
+    password = password.trim()
+    dateOfbirth = dateOfbirth.trim()
+
+    if(fullname == "" || username == "" || email == "" || password == "" || dateOfbirth == ""){
+        res.status(400).json({
+            message: "All input are required! Please make sure you complete each field with the required data"
+        })
+    }
+})
+
 module.exports = router
