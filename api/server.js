eeebf95cc2e692e22cd19b87c0f9181e93a7e012
Bishop ukpai <bishopukpai0@gmail.com>
2024-08-27 10:25:35 +0100

    created the route endpoints

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ require('./config/databaseConfig')
 const helmet = require('helmet');
 const morgan = require('morgan')
 
+const userRoute = require('./routes/User')
+const signupRoute = require('./routes/Signup')
+const signinRoute = require('./routes/Signin')
 
 const app = express()
 
@@ -11,6 +14,10 @@ const port = 9090;
 app.use(express.json())
 app.use(helmet())
 app.use(morgan("dev"))
+
+app.use("/api/user", userRoute)
+app.use('/api/auth', signupRoute)
+app.use('/api/auth', signinRoute)
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
