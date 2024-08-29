1b2eebc9e7265152cf59a7d20e657c981523d985
Bishop ukpai <bishopukpai0@gmail.com>
2024-08-29 07:30:09 +0100

    I created the complete logic for the otp verification and I used the otp verification route inside the server.js file

--- api/routes/VerifyOTP.js
+++ api/routes/VerifyOTP.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const route = express.Router()
+const OTPVerification = require('../models/OTPVerification')
+const User = require('../models/Usermodels')
+const bcrypt = require('bcrypt')
 
 route.post("/verifyotp", async(req, res) => {
     try{
@@ -9,6 +12,43 @@ route.post("/verifyotp", async(req, res) => {
             res.status(400).json({
                 message: "Please provide a one-time password!"
             })
+        }else {
+            const OTPVerificationRecord = await OTPVerification({userId})
+
+            if(OTPVerificationRecord.length <= 0){
+                res.status(400).json({
+                    message: "Invalid Verification details!"
+                })
+            }else {
+                const {expiresAt} = OTPVerificationRecord[0]
+                const hashedOTP = OTPVerificationRecord[0].otp
+
+                if(expiresAt < Date.now()){
+                    await OTPVerification.deleteMany({userId})
+
+                    await User.deleteOne({_id:userId})
+
+                    res.status(400).json({
+                        message: "One time password has expired! Please signup to get another one"
+                    })
+                }else {
+                    const validOTP = await bcrypt.compare(otp, hashedOTP)
+
+                    if(!validOTP){
+                        res.status(400).json({
+                            message: "Invalid one time password!"
+                        })
+                    }else {
+                        await User.updateOne({_id:userId}, {verified:true})
+
+                        await OTPVerification.deleteMany(userId)
+
+                        res.status(200).json({
+                            message: "Verified!"
+                        })
+                    }
+                }
+            }
         }
     }catch (error){
         res.status(500).json({
@@ -16,3 +56,5 @@ route.post("/verifyotp", async(req, res) => {
         })
     }
 })
+
+module.exports = route
--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 const userRoute = require('./routes/User')
 const signupRoute = require('./routes/Signup')
 const signinRoute = require('./routes/Signin')
+const verifyotpRoute = require('./routes/VerifyOTP')
 
 const app = express()
 
@@ -18,6 +19,7 @@ app.use(morgan("dev"))
 app.use("/api/user", userRoute)
 app.use('/api/auth', signupRoute)
 app.use('/api/auth', signinRoute)
+app.use("/api/user", verifyotpRoute)
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
