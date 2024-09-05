bd4a89b63144433bf57cba5ec10c262fb85790a3
Bishop ukpai <bishopukpai0@gmail.com>
2024-09-05 18:13:19 +0100

    added the endpoint for users to delete their account

--- api/routes/User.js
+++ api/routes/User.js
@@ -30,6 +30,28 @@ router.put("/updateprofile/:id", async(req, res) => {
                 message: "Internal Server error"
             })
         }
+    }else {
+        res.status(403).json({
+            message: "You cannot update this profile"
+        })
+    }
+})
+
+router.delete("/deleteprofile/:id", async(req, res) => {
+    let {userId} = req.body;
+
+    if(userId === req.params.id){
+        try{
+            const user = await User.findByIdAndDelete(userId)
+
+            res.status(200).json({
+                message: "Account deleted!"
+            })
+        }catch(error){
+            res.status(500).json({
+                message: "Internal error"
+            })
+        }
     }
 })
 
