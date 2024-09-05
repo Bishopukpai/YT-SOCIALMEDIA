6c66ebe5a6381b45448611ec2ef42b213612a483
Bishop ukpai <bishopukpai0@gmail.com>
2024-09-05 15:36:43 +0100

    added the endpoint for fetching the user profile data

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,3 +1,18 @@
 const router = require('express').Router()
+const User = require('../models/Usermodels')
+
+router.get("/:id", async(req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+
+        const {password, updatedAt, verified, ...other} = user._doc
+
+        res.status(200).json(other)
+    }catch(error){
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+})
 
 module.exports = router
