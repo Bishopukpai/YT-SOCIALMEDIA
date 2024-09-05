78f5225b012f175bcaf709ff729e143b419ce30a
Bishop ukpai <bishopukpai0@gmail.com>
2024-09-05 16:34:09 +0100

    Added the endpoint for updating user profile

--- api/routes/User.js
+++ api/routes/User.js
@@ -15,4 +15,22 @@ router.get("/:id", async(req, res) => {
     }
 })
 
+router.put("/updateprofile/:id", async(req, res) => {
+    let {userId} = req.body;
+
+    if(userId === req.params.id){
+        try{
+            const user = await User.findByIdAndUpdate(userId, {$set: req.body})
+
+            res.status(200).json({
+                message: "Profile updated!"
+            })
+        }catch(error){
+            res.status(500).json({
+                message: "Internal Server error"
+            })
+        }
+    }
+})
+
 module.exports = router
