c91d3df61b98881452dfc1bac0c665b0e916ddda
Bishop ukpai <bishopukpai0@gmail.com>
2024-09-06 11:22:51 +0100

    created the post schema model

---
+++ api/models/Postmodel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+    userId: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        max: 5000
+    },
+    image: {
+        type: String,
+    },
+    like: {
+        type: Array,
+        default: []
+    },
+    comments: {
+        type: Array,
+        default: []
+    }
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model("Post", PostSchema)
